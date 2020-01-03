6bd549d4f0ec88dbb024fe0b3e40db73c94d6388
Elaiyakumar <elaiyakumar@hotmail.com>
2020-01-03 07:29:58 +0000

    Add project files. Autofac WIP

---
+++ GoldStore.Core/Domain/Store/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GoldStore.Core.Domain.Store
+{
+    public class Order : BaseEntity
+    {        
+        public string OrderCode { get; set; }
+        public int PoductId { get; set; }
+        public int Quantity { get; set; }
+        public double Cost { get; set; }
+        public DateTime OrderDate { get; set; }
+        //TODO : OrderItem         
+    }
+}
---
+++ GoldStore.Core/Domain/Store/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GoldStore.Core.Domain.Store
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int BatchNum { get; set; }
+        public double UnitPrice { get; set; }
+        public DateTime ExpiryDate  { get; set; }
+        public int Count { get; set; }
+    }
+}
