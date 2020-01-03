76fea18e2411179de250a8a67fd50e7526977e3f
Elaiyakumar <elaiyakumar@hotmail.com>
2020-01-03 08:23:24 +0000

    AutoMapper added wip

---
+++ GoldStore.Core/Infrastructure/Mapper/IMapperConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+
+namespace GoldStore.Core.Infrastructure.Mapper
+{
+    /// <summary>
+    /// Mapper configuration registrar interface
+    /// </summary>
+    public interface IMapperConfiguration
+    {
+        /// <summary>
+        /// Get configuration
+        /// </summary>
+        /// <returns>Mapper configuration action</returns>
+        Action<IMapperConfigurationExpression> GetConfiguration();
+
+        /// <summary>
+        /// Order of this mapper implementation
+        /// </summary>
+        int Order { get; }
+    }
+}
